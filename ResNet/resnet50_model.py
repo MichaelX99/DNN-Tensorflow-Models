8b951ce4c5594467e00d013900aab6a72b46e805
Michael Person <mp.michael.person@gmail.com>
2017-08-03 18:05:04 -0400

    working 50 layer resnet with bottleneck but I don't think my architecture is right

--- ResNet/resnet50_model.py
+++ ResNet/resnet50_model.py
@@ -10,7 +10,7 @@ def inference(images):
     pool1 = tf.nn.max_pool(conv1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1],
                         padding='SAME', name='pool1')
 
-    rescale1 = helper.convolution(pool1, [1, 1, 64, 256], helper.DECAY, 2, 'rescale1')
+    rescale1 = helper.convolution(pool1, [1, 1, 64, 256], helper.DECAY, 1, 'rescale1')
 
     # conv2_B
     conv2_B = helper.bottleneck_convolution(pool1, 64, [3,3,64,256], helper.DECAY, 1, 'conv2_B') + rescale1
@@ -19,13 +19,15 @@ def inference(images):
     conv3_B = helper.bottleneck_convolution(conv2_B, 64, [3,3,256,256], helper.DECAY, 1, 'conv3_B') + conv2_B
 
     # conv4_B
-    conv4_B = helper.bottleneck_convolution(conv3_B, 64, [3,3,256,256], helper.DECAY, 2, 'conv4_B') + conv3_B
+    conv4_B = helper.bottleneck_convolution(conv3_B, 64, [3,3,256,256], helper.DECAY, 2, 'conv4_B') + helper.convolution(conv3_B, [1, 1, 256, 256], helper.DECAY, 2, 'block1')
 
     # projection1
     proj1 = helper.projection(conv3_B, conv4_B, helper.DECAY, 'proj1')
 
+    rescale2 = helper.convolution(proj1, [1, 1, 256, 512], helper.DECAY, 1, 'rescale2')
+
     # conv5_B
-    conv5_B = helper.bottleneck_convolution(proj1, 128, [3,3,256,512], helper.DECAY, 1, 'conv5_B') + proj1
+    conv5_B = helper.bottleneck_convolution(proj1, 128, [3,3,256,512], helper.DECAY, 1, 'conv5_B') + rescale2
 
     # conv6_B
     conv6_B = helper.bottleneck_convolution(conv5_B, 128, [3,3,512,512], helper.DECAY, 1, 'conv6_B') + conv5_B
@@ -34,14 +36,15 @@ def inference(images):
     conv7_B = helper.bottleneck_convolution(conv6_B, 128, [3,3,512,512], helper.DECAY, 1, 'conv7_B') + conv6_B
 
     # conv8_B
-    conv8_B = helper.bottleneck_convolution(conv7_B, 128, [3,3,512,512], helper.DECAY, 2, 'conv8_B') + conv7_B
+    conv8_B = helper.bottleneck_convolution(conv7_B, 128, [3,3,512,512], helper.DECAY, 2, 'conv8_B') + helper.convolution(conv7_B, [1, 1, 512, 512], helper.DECAY, 2, 'block2')
 
     # projection2
     proj2 = helper.projection(conv7_B, conv8_B, helper.DECAY, 'proj2')
 
+    rescale3 = helper.convolution(proj2, [1, 1, 512, 1024], helper.DECAY, 1, 'rescale3')
 
     # conv9_B
-    conv9_B = helper.bottleneck_convolution(proj2, 256, [3,3,512,1024], helper.DECAY, 1, 'conv9_B') + proj2
+    conv9_B = helper.bottleneck_convolution(proj2, 256, [3,3,512,1024], helper.DECAY, 1, 'conv9_B') + rescale3
 
     # conv10_B
     conv10_B = helper.bottleneck_convolution(conv9_B, 256, [3,3,1024,1024], helper.DECAY, 1, 'conv10_B') + conv9_B
@@ -56,19 +59,22 @@ def inference(images):
     conv13_B = helper.bottleneck_convolution(conv12_B, 256, [3,3,1024,1024], helper.DECAY, 1, 'conv13_B') + conv12_B
 
     # conv14_B
-    conv14_B = helper.bottleneck_convolution(conv13_B, 256, [3,3,1024,1024], helper.DECAY, 2, 'conv14_B') + conv13_B
+    conv14_B = helper.bottleneck_convolution(conv13_B, 256, [3,3,1024,1024], helper.DECAY, 2, 'conv14_B') + helper.convolution(conv13_B, [1, 1, 1024, 1024], helper.DECAY, 2, 'block3')
 
     # projection3
     proj3 = helper.projection(conv13_B, conv14_B, helper.DECAY, 'proj3')
 
+    rescale4 = helper.convolution(proj3, [1, 1, 1024, 2048], helper.DECAY, 1, 'rescale4')
+
     # conv15_B
-    conv15_B = helper.bottleneck_convolution(proj3, 512, [3,3,1024,2048], helper.DECAY, 1, 'conv15_B') + proj3
+    conv15_B = helper.bottleneck_convolution(proj3, 512, [3,3,1024,2048], helper.DECAY, 1, 'conv15_B') + rescale4
 
     # conv16_B
     conv16_B = helper.bottleneck_convolution(conv15_B, 512, [3,3,2048,2048], helper.DECAY, 1, 'conv16_B') + conv15_B
 
     # conv17_B
-    conv17_B = helper.bottleneck_convolution(conv16_B, 512, [3,3,2048,2048], helper.DECAY, 2, 'conv17_B') + conv16_B
+    conv17_B = helper.bottleneck_convolution(conv16_B, 512, [3,3,2048,2048], helper.DECAY, 2, 'conv17_B') + helper.convolution(conv16_B, [1, 1, 2048, 2048], helper.DECAY, 2, 'block4')
+
 
     # projection4
     proj4 = helper.projection(conv16_B, conv17_B, helper.DECAY, 'proj4')
